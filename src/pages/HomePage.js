995449d0362093eacdf8b9cd0332d71cff9fadcf
AMAR KISHORE SENAPATI <amarsenapati4@gmail.com>
2024-02-21 12:34:30 +0530

    update homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import Layout from '../components/Layout/Layout'
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout/Layout";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
 import { useCart } from "../Context/cart";
 import toast from "react-hot-toast";
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx';
-
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 const HomePage = () => {
   const slides = [
     {
-      url: 'https://shop.teamsg.in/wp-content/uploads/2023/07/200-DPI_Player_Banner-2.png',
+      url: "https://shop.teamsg.in/wp-content/uploads/2023/07/200-DPI_Player_Banner-2.png",
     },
     {
-      url: 'https://static.vecteezy.com/system/resources/previews/001/330/144/original/realistic-soccer-or-football-in-net-with-copy-space-free-vector.jpg',
+      url: "https://static.vecteezy.com/system/resources/previews/001/330/144/original/realistic-soccer-or-football-in-net-with-copy-space-free-vector.jpg",
     },
     {
-      url: 'https://cdn.fitimg.in/blog_banner_B9CCE7A75859AEF.png',
+      url: "https://cdn.fitimg.in/blog_banner_B9CCE7A75859AEF.png",
     },
 
     {
-      url: 'https://sportshub.cbsistatic.com/i/r/2023/07/12/f77309e6-c639-46a2-8142-727c9cadd73d/thumbnail/1200x675/6cedf57667583332df1b598788d44b23/treadmill-hero.png',
+      url: "https://sportshub.cbsistatic.com/i/r/2023/07/12/f77309e6-c639-46a2-8142-727c9cadd73d/thumbnail/1200x675/6cedf57667583332df1b598788d44b23/treadmill-hero.png",
     },
     {
-      url: 'https://www.sstoncricket.com/wp-content/uploads/2023/07/kit-banner2.jpg',
+      url: "https://www.sstoncricket.com/wp-content/uploads/2023/07/kit-banner2.jpg",
     },
   ];
 
@@ -60,7 +59,9 @@ const HomePage = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("https://ecommerce-backend-us2n.onrender.com/api/v1/category/get-category");
+      const { data } = await axios.get(
+        "https://ecommerce-backend-us2n.onrender.com/api/v1/category/get-category"
+      );
       if (data?.success) {
         setCategories(data?.category);
       }
@@ -74,38 +75,37 @@ const HomePage = () => {
     getTotal();
   }, []);
   //get products
- 
 
   //getTOtal COunt
   const getTotal = async () => {
     try {
-      const { data } = await axios.get("https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-count");
+      const { data } = await axios.get(
+        "https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-count"
+      );
       setTotal(data?.total);
     } catch (error) {
       console.log(error);
     }
   };
 
- 
   useEffect(() => {
-
-     
-  const loadMore = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.get(`https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-list/${page}`);
-      setLoading(false);
-      setProducts([...products, ...data?.products]);
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-  };
+    const loadMore = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          `https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-list/${page}`
+        );
+        setLoading(false);
+        setProducts([...products, ...data?.products]);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    };
     if (page === 1) return;
     loadMore();
-  }, [page,products]);
+  }, [page, products]);
   //load more
-
 
   // filter by cat
   const handleFilter = (value, id) => {
@@ -118,11 +118,12 @@ const HomePage = () => {
     setChecked(all);
   };
   useEffect(() => {
-
     const getAllProducts = async () => {
       try {
         setLoading(true);
-        const { data } = await axios.get(`https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-list/${page}`);
+        const { data } = await axios.get(
+          `https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-list/${page}`
+        );
         setLoading(false);
         setProducts(data.products);
       } catch (error) {
@@ -131,16 +132,18 @@ const HomePage = () => {
       }
     };
     if (!checked.length || !radio.length) getAllProducts();
-  }, [checked.length, radio.length,page]);
+  }, [checked.length, radio.length, page]);
 
   useEffect(() => {
-
     const filterProduct = async () => {
       try {
-        const { data } = await axios.post("https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-filters", {
-          checked,
-          radio,
-        });
+        const { data } = await axios.post(
+          "https://ecommerce-backend-us2n.onrender.com/api/v1/product/product-filters",
+          {
+            checked,
+            radio,
+          }
+        );
         setProducts(data?.products);
       } catch (error) {
         console.log(error);
@@ -150,39 +153,15 @@ const HomePage = () => {
   }, [checked, radio]);
 
   //get filterd product
- 
 
   return (
     <Layout title={"ALl Products - Best offers "}>
       <div className="container-fluid row  mt-[3rem] justify-between">
-      <div className='max-w-[1000px]  h-[350px] md:max-w-[1380px] md:h-[600px] w-full m-auto py-16 px-4 relative group'>
-      <div
-        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
-      ></div>
-      {/* Left Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
-      {/* Right Arrow */}
-      <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className='flex top-4 justify-center py-2'>
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className='text-2xl cursor-pointer'
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
-    </div>
-        <div className='flex md:flex-row flex-col gap-4'>
+        <div className="flex md:flex-row flex-col gap-4">
           <div className="col-md-2">
-            <h4 className="text-center font-bold text-emerald-700 mt-5">Filter By Category</h4>
+            <h4 className="text-center font-bold text-emerald-700 mt-5">
+              Filter By Category
+            </h4>
             <div className="d-flex flex-column">
               {categories?.map((c) => (
                 <Checkbox
@@ -194,7 +173,9 @@ const HomePage = () => {
               ))}
             </div>
             {/* price filter */}
-            <h4 className="text-center mt-4 font-bold text-emerald-700">Filter By Price</h4>
+            <h4 className="text-center mt-4 font-bold text-emerald-700">
+              Filter By Price
+            </h4>
             <div className="d-flex flex-column">
               <Radio.Group onChange={(e) => setRadio(e.target.value)}>
                 {Prices?.map((p) => (
@@ -214,7 +195,9 @@ const HomePage = () => {
             </div>
           </div>
           <div className="col-md-9">
-            <h1 className="text-center text-3xl mb-2 text-red-600">All Products</h1>
+            <h1 className="text-center text-3xl mb-2 text-red-600">
+              All Products
+            </h1>
             <div className="flex flex-wrap w-full justify-center items-center">
               {products?.map((p) => (
                 <div className="card m-3" style={{ width: "18rem" }}>
@@ -229,26 +212,40 @@ const HomePage = () => {
                       {p.description.substring(0, 30)}...
                     </p>
                     <p className="card-text"> ₹ {p.price}</p>
-                    <h5 className="card-title">Status:{p.quantity>0 ? 'IN STOCK':'OUT OF STOCK'}</h5>
-                    <div className='flex flex-row'>
-                    <button class="btn btn-primary ms-1" onClick={() => navigate(`/product/${p.slug}`)}>More Details</button>
-                    {p.quantity>0 ? (
-                      <>
-                          <button class="btn btn-secondary ms-1" onClick={() => {
-                      setCart([...cart, p]);
-                      localStorage.setItem(
-                        "cart",
-                        JSON.stringify([...cart, p])
-                      );
-                      toast.success("Item Added to cart");
-                    }}>ADD TO CART</button>
-                      </>
-                    ):(
-                      <>
-                          <button class="btn btn-secondary ms-1 " disabled>ADD TO CART</button>
-                      </>
-                    )}
-                </div>
+                    <h5 className="card-title">
+                      Status:{p.quantity > 0 ? "IN STOCK" : "OUT OF STOCK"}
+                    </h5>
+                    <div className="flex flex-row">
+                      <button
+                        class="btn btn-primary ms-1"
+                        onClick={() => navigate(`/product/${p.slug}`)}
+                      >
+                        More Details
+                      </button>
+                      {p.quantity > 0 ? (
+                        <>
+                          <button
+                            class="btn btn-secondary ms-1"
+                            onClick={() => {
+                              setCart([...cart, p]);
+                              localStorage.setItem(
+                                "cart",
+                                JSON.stringify([...cart, p])
+                              );
+                              toast.success("Item Added to cart");
+                            }}
+                          >
+                            ADD TO CART
+                          </button>
+                        </>
+                      ) : (
+                        <>
+                          <button class="btn btn-secondary ms-1 " disabled>
+                            ADD TO CART
+                          </button>
+                        </>
+                      )}
+                    </div>
                   </div>
                 </div>
               ))}
@@ -270,7 +267,7 @@ const HomePage = () => {
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
